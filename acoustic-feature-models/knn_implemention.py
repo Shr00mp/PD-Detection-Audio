3d30ed7d875b55c6bf358600b2adcc5f348676fb
Sharon <theewimpyshrimp@gmail.com>
2025-10-24 19:04:56 +0100

    Started knn implementation

--- acoustic-feature-models/knn_implemention.py
+++ acoustic-feature-models/knn_implemention.py
@@ -1,2 +1,9 @@
 import torch 
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv("acoustic-feature-models/audio_features.csv")
+X = df.drop(columns=["Sample ID", "Label"]) # features
+Y = df["Label"] # labels
