9107ce50445814e128850eac5bf9944cea330911
Sharon <147839877+Shr00mp@users.noreply.github.com>
2025-10-25 12:48:57 +0100

    Fixed feature selection bugs

--- acoustic-feature-models/KNN/knn_feature_selection.py
+++ acoustic-feature-models/KNN/knn_feature_selection.py
@@ -12,21 +12,21 @@ from sklearn.model_selection import train_test_split
 
 # filter method using chi-square test
 def univariate_selection(X_train, y_train, X_test):
-    select_k_best = SelectKBest(score_func=f_classif, k=10)
+    select_k_best = SelectKBest(score_func=f_classif, k=15)
     # fit to x_train + transform
     X_train_best = select_k_best.fit_transform(X_train, y_train)
     # DON'T fit to x_test, ONLY transform 
     X_test_best = select_k_best.transform(X_test)
-    print(f"Selected features: {X_train.columns[select_k_best.get_support()]}")
+    print("Filter method selected features:\n" + "\n".join(X_train.columns[select_k_best.get_support()]))
     return X_train_best, X_test_best
 
 # wrapper method with logistic regression
 def recursive_selection(X_train, y_train, X_test):
     model = LogisticRegression()
-    rfe = RFE(model, n_features_to_select=10)
+    rfe = RFE(model, n_features_to_select=15)
     X_train_rfe = rfe.fit_transform(X_train, y_train)
     X_test_rfe = rfe.transform(X_test)
-    print(f"Selected features: {X_train.columns[rfe.get_support()]}")
+    print("Wrapper method selected features:\n" + "\n".join(X_train.columns[rfe.get_support()]))
     return X_train_rfe, X_test
 
 # embedded method with random forest 
@@ -38,22 +38,24 @@ def rf_selection(X_train, y_train):
 
     # Print importances of features in decreasing order
     feature_importances = pd.Series(importances, index=X_train.columns)
+    print("\nImportance of features using rf: ")
     print(feature_importances.sort_values(ascending=False))
 
+# Get all training and target data
 df = pd.read_csv("acoustic-feature-models/audio_features.csv")
-X = df.drop(columns=["Sample ID", "Label"]) # features
-Y = df["Label"] # labels
-
+X_df = df.drop(columns=["Sample ID", "Label"]) # features
+Y_df = df["Label"] # labels
 
 # z-score normalisation
 scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+X_scaled = scaler.fit_transform(X_df)
+X_scaled_df = pd.DataFrame(X_scaled, columns=X_df.columns)
 
 # training and testing split of 80-20 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X_scaled_df, Y_df, test_size=0.2, random_state=42)
 
 # Filter method
-X_train_best, X_test_best = univariate_selection(X_train, y_train, X_test)
-
-X = X.to_numpy()
-Y = Y.to_numpy()
+X_train_best_f, X_test_best_f = univariate_selection(X_train, y_train, X_test)
+X_train_best_w, X_test_best_w = recursive_selection(X_train, y_train, X_test)
+rf_selection(X_train, y_train)
