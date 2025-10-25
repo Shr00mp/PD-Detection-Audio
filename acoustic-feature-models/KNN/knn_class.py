489fb7bca0f30ce55c0cf7cce77b76c8345a81a7
Sharon <147839877+Shr00mp@users.noreply.github.com>
2025-10-25 22:50:59 +0100

    Continuing KNN class, error with recursive selection

--- acoustic-feature-models/KNN/knn_class.py
+++ acoustic-feature-models/KNN/knn_class.py
@@ -11,12 +11,18 @@ from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 
 class KNNModel:
-    def __init__(self, X, Y):
+    def __init__(self, X, Y, do_normalisation, do_feature_selection,
+                 feature_selection_type):
         self = self
         self.X = X
         self.Y = Y
+        self.do_normalisation = do_normalisation
+        self.do_feature_selection = do_feature_selection
+        self.feature_selection_type = feature_selection_type
+        self.best_k = None
+        self.best_accuracy = None
     
-    def normalise(X_df):
+    def normalise(self, X_df):
         # z-score normalisation
         scaler = StandardScaler()
         X_scaled = scaler.fit_transform(X_df)
@@ -24,26 +30,30 @@ class KNNModel:
         return X_scaled_df
     
     # filter method with f_classif test
-    def univariate_selection(X_train, y_train, X_test):
+    def univariate_selection(self, X_train, y_train, X_test, get_features):
         select_k_best = SelectKBest(score_func=f_classif, k=15)
         # fit to x_train + transform
         X_train_best = select_k_best.fit_transform(X_train, y_train)
         # DON'T fit to x_test, ONLY transform 
         X_test_best = select_k_best.transform(X_test)
         print("Filter method selected features:\n" + "\n".join(X_train.columns[select_k_best.get_support()]))
-        return X_train_best, X_test_best
+        if (get_features): return X_train.columns[select_k_best.get_support()]
+        else: return X_train_best, X_test_best
 
     # wrapper method with logistic regression
-    def recursive_selection(X_train, y_train, X_test):
+    def recursive_selection(self, X_train, y_train, X_test, get_features):
         model = LogisticRegression()
         rfe = RFE(model, n_features_to_select=15)
-        X_train_rfe = rfe.fit_transform(X_train, y_train)
-        X_test_rfe = rfe.transform(X_test)
+        X_train_best = rfe.fit_transform(X_train, y_train)
+        X_test_best = rfe.transform(X_test)
+        # Note: rfe.get_support() is a boolean mask
+        # X_train.columns[rfe.get_support()] has datatype pandas.Index 
         print("Wrapper method selected features:\n" + "\n".join(X_train.columns[rfe.get_support()]))
-        return X_train_rfe, X_test
+        if (get_features): return X_train.columns[rfe.get_support()]
+        else: return X_train_best, X_test_best
     
     # embedded method with random forest 
-    def rf_selection(X_train, y_train):
+    def rf_selection(self, X_train, y_train, X_test, get_features):
         # Train rf and get feature importances
         model = RandomForestClassifier()
         model.fit(X_train, y_train)
@@ -51,19 +61,40 @@ class KNNModel:
 
         # Print importances of features in decreasing order
         feature_importances = pd.Series(importances, index=X_train.columns)
+        sorted_importances = feature_importances.sort_values(ascending=False)
         print("\nImportance of features using rf: ")
-        print(feature_importances.sort_values(ascending=False))
+        print(sorted_importances)
+
+        # Get and return x_train and x_test
+        top_features = sorted_importances.head(15)
+        top_feature_names = top_features.index
+        X_train_best = X_train[top_feature_names]
+        X_test_best = X_test[top_feature_names]
+        if (get_features): return top_feature_names
+        else: return X_train_best, X_test_best
+    
+    def intersection_selection(self, X_train, y_train, X_test):
+        # Each variable below has data type pandas.Index
+        univariate_features = self.univariate_selection(X_train, y_train, X_test, get_features=True)
+        recursive_features = self.univariate_selection(X_train, y_train, X_test, get_features=True)
+        rf_features = self.rf_selection(X_train, y_train, X_test, get_features=True)
+
+        combined_indexes = univariate_features.union(recursive_features).union(rf_features)
+        X_train_best = X_train[combined_indexes]
+        X_test_best = X_test[combined_indexes]
+        return X_train_best, X_test_best
+
     
     # model with k neighbours. returns mean accuracy
-    def evaluate_k(self, k, do_normalisation, do_fs, fs_type, n_folds):
+    def evaluate_model(self, k):
         X_df = self.X
         Y_df = self.Y
         # normalisation
-        if (do_normalisation): X_df = self.normalise(X_df)
+        if (self.do_normalisation): X_df = self.normalise(X_df)
         Y_df.to_numpy()
 
         # Creates KFold object that divides dataset indices into n=5 folds
-        kf = KFold(n_splits=n_folds, shuffle=True, random_state=42)
+        kf = KFold(n_splits=5, shuffle=True, random_state=42)
         fold_accuracies = []
         fold_precisions = []
         fold_recalls = []
@@ -75,15 +106,19 @@ class KNNModel:
             y_train = Y_df[train_index]
             y_test = Y_df[test_index]
 
-            if (do_fs) :
-                if (fs_type == "uni"):
-                    X_train, X_test = self.univariate_selection(X_train, y_train, X_test)
-                elif (fs_type == "recursive"):
-                    X_train, X_test = self.recursive_selection(X_train, y_train, X_test)
-                elif (fs_type == "rf"):
-                    X_train, X_test = self.rf_selection(X_train, y_train, X_test)
-                elif (fs_type == "overlap"):
-                    pass
+            if (self.do_feature_selection) :
+                if (self.feature_selection_type == "uni"):
+                    X_train, X_test = self.univariate_selection(X_train, y_train, X_test, get_features=False)
+                elif (self.feature_selection_type == "recursive"):
+                    X_train, X_test = self.recursive_selection(X_train, y_train, X_test, get_features=False)
+                elif (self.feature_selection_type == "rf"):
+                    X_train, X_test = self.rf_selection(X_train, y_train, X_test, get_features=False)
+                elif (self.feature_selection_type == "intersection"):
+                    X_train, X_test = self.intersection_selection(X_train, y_train, X_test)
+                else: 
+                    print("Feature selection method does not exist. " \
+                "\nChoose either uni, recursive, rf or intersection.")
+                    return
 
             knn = KNeighborsClassifier(n_neighbors=k)
             knn.fit(X_train, y_train) # fits model using training data
@@ -110,11 +145,16 @@ class KNNModel:
 
         return mean_accuracy
     
-    def plot_k_accuracy(self, k_max, do_normalisation, do_fs, fs_type, n_folds):
+    def plot_k_accuracy(self, k_max):
         accuracies = []
         k_values = range(1, k_max)
+        self.best_k = 1
+        self.best_accuracy = 0
         for k in k_values:
-            acc_score = self.evaluate_k(k, do_normalisation, do_fs, fs_type, n_folds)
+            acc_score = self.evaluate_model(k)
+            if (acc_score > self.best_accuracy): 
+                self.best_accuracy = acc_score
+                self.best_k = k
             accuracies.append(acc_score)
 
         plt.figure(figsize=(8, 5))
@@ -130,4 +170,6 @@ df = pd.read_csv("acoustic-feature-models/audio_features.csv")
 X = df.drop(columns=["Sample ID", "Label"])  # Features
 Y = df["Label"]  # Labels
 
-knn_model = KNNModel(X, Y)
+knn_model = KNNModel(X, Y, True, False, "rf")
+knn_model.plot_k_accuracy(30)
+print(f"\nBest k: {knn_model.best_k} \nAccuracy: {knn_model.best_accuracy}")
